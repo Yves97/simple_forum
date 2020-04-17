962a00f6dfb8c254a27ff28942a0e368cc489361
yvnan2.19 <yveskouadio111@yahoo.fr>
2020-04-17 00:03:34 +0000

    tchat

--- views/includes/header.php
+++ views/includes/header.php
@@ -1,9 +1,10 @@
 <!-- ce fichier contient le header du site -->
 <header>
-    <nav class="navbar">
+    <nav class="navbar mb-3">
         <!--modal profil-->
         <a href="<?= $page="profil" ?>">
             <img src="assets/images/photo.png" class="img-fluid img-profil" alt="photo de profile">
         </a>
+        <button class="btn-edit">Déconnection</button>
     </nav>
 </header>
