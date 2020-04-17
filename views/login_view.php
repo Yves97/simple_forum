17f738dcacdf71c6a93bfc89a2ed3e261b22e986
yvnan2.19 <yveskouadio111@yahoo.fr>
2020-04-17 12:57:19 +0000

    login

---
+++ views/login_view.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include_once 'views/includes/head.php' ?>
+    <title><?= $page ?></title>
+</head>
+<body>
+    <div class="main-card d-flex justify-content-center align-items-center">
+        <div class="box-form">
+            <hr class="divider">
+            <h3>Login</h3>
+            <form action="">
+                <input class="input-edit" type="text" name="" placeholder="Pseudo">
+                <input class="input-edit" type="password" name="" id="" placeholder="Password">
+            </form>
+        </div>
+    </div>
+</body>
