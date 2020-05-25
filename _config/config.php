8430a7c4e4bebbe51cbd49f0e4c245b8667915c1
yvnan2.19 <yveskouadio111@yahoo.fr>
2020-05-25 15:37:48 +0000

    remove substr

--- _config/config.php
+++ _config/config.php
@@ -26,7 +26,7 @@ session_start();
 // --------------------------- //
 
 // Paths
-define("PATH_REQUIRE", substr($_SERVER['SCRIPT_FILENAME'], 0, -9)); // Pour fonctions d'inclusion php
+define("PATH_REQUIRE", $_SERVER['SCRIPT_FILENAME']); // Pour fonctions d'inclusion php
 define("PATH", substr($_SERVER['PHP_SELF'], 0, -9)); // Pour images, fichiers etc (html)
 
 // Website informations
